b81e59b1650170ac12ddea03e3ab01710903037f
pr3mar <markoprelevik@gmail.com>
2017-10-23 20:59:26 +0200

    initial commit

---
+++ src/main/java/com/rsoStream/dummyProducer/ProducerApp.java
@@ -0,0 +1,9 @@
+package com.rsoStream.dummyProducer;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("v1")
+public class ProducerApp extends Application {
+
+}
